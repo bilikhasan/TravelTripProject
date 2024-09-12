ec269bee6a099901d27739e59827f28815852632
bilikhasan <fatherlyy@hotmail.com.tr>
2024-09-12 23:39:47 +0300

    PartialView1 - PartialView2 was added.

--- TravelTripProject/Controllers/BlogController.cs
+++ TravelTripProject/Controllers/BlogController.cs
@@ -12,15 +12,23 @@ namespace TravelTripProject.Controllers
     {
         // GET: Blog
         Context c = new Context();
+
+        BlogYorum by = new BlogYorum();
+
         public ActionResult Index()
         {
-            var bloglar = c.Blogs.ToList();
-            return View(bloglar);
+            //var bloglar = c.Blogs.ToList();
+            by.Deger1 = c.Blogs.ToList();
+            by.Deger3 = c.Blogs.OrderByDescending(x => x.ID).Take(3).ToList();           //Son bloglardan 3 tanesını alacak.
+
+            return View(by);
         }
         public ActionResult BlogDetay(int id)
         {
-            var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
-            return View(blogbul);
+            //var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger2 = c.Yorumlars.Where(x => x.Blogid == id).ToList();
+            return View(by);
         }
     }
 }
